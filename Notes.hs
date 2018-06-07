8603fe2afac272f3bfce00caf92610581175b7d9
Kate Dawson <kate@webarchitects.coop>
2018-06-07 18:04:19 +0100

    extended it a  bit

--- Notes.hs
+++ Notes.hs
@@ -22,6 +22,8 @@ a1 = Note { pitch = (Just P.a1), voice = Nothing, duration = Nothing, envelope =
 e1 = Note { pitch = (Just P.e1), voice = Nothing, duration = Nothing, envelope = Nothing }
 f1 = Note { pitch = (Just P.f1), voice = Nothing, duration = Nothing, envelope = Nothing }
 a2 = Note { pitch = (Just P.a2), voice = Nothing, duration = Nothing, envelope = Nothing }
+ne3 = Note { pitch = (Just P.e3), voice = Nothing, duration = Nothing, envelope = Nothing }
+af3 = Note { pitch = (Just P.af3), voice = Nothing, duration = Nothing, envelope = Nothing }
 a3 = Note { pitch = (Just P.a3), voice = Nothing, duration = Nothing, envelope = Nothing }
 a4 = Note { pitch = (Just P.a4), voice = Nothing, duration = Nothing, envelope = Nothing }
 
@@ -53,6 +55,7 @@ e2 = map setEnvelope [constEnv2, constEnv3, constEnv2]
 -- p2 = concat $ replicate  16 $  map (setEnvelope constEnv1) v2
 p2 = concat $ replicate  8 $  zipWith ($) (concat $ repeat e2) v2
 p4 = p2 ++ p2 
+
 note3Map = map noteTranspose [0,0,0,5,5,5,-2,-3,-4,0,0,0]
 notes3 = note3Map <*> [a4]
 durMap3 = map setDuration $ [D.n16, D.n16, D.n8]
@@ -60,11 +63,21 @@ voiceMap3 = map setVoice $ (replicate 2 silence ) ++ (replicate 4 minorSquareSta
 -- e3 = map setEnvelope [bwap2Env3, constEnv3, constEnv0, short1Env3, constEnv0]
 e3 = map setEnvelope [bwap2Env3, short1Env3, constEnv0]
 
-
 d3 = zipWith ($) (concat $ repeat durMap3) notes3
 v3 = (voiceMap3) <*> d3
-p3 = zipWith ($) (concat $ repeat e3) v3
+p3 = zipWith ($) (concat $ repeat e4) v3
 
+note4Map = map noteTranspose [0]
+notes4 = note4Map <*> [a3]
+durMap4 = reverse $ map setDuration $ [D.n8t]
+-- durMap4 = reverse $ map setDuration $ [D.n16, D.n16, D.n8, D.n8t, D.n8t, D.n8t]
+voiceMap4 = map setVoice $ (replicate 2 silence ) ++ (replicate 4 minorSquareStab) ++ (replicate 2 silence)
+-- e3 = map setEnvelope [bwap2Env3, constEnv3, constEnv0, short1Env3, constEnv0]
+e4 = map setEnvelope [bwap2Env3, short1Env3, constEnv0]
+
+d4 = zipWith ($) (concat $ repeat durMap4) notes4
+v4 = (voiceMap3) <*> d4
+p5 = zipWith ($) (concat $ repeat e4) v4
 
 noteToDoubles :: Note -> Maybe [Double]
 noteToDoubles n =  
@@ -94,7 +107,6 @@ hardFlange n  xs =
     do {  ds <- xs;
         return $ normalize $ mix ds $ (replicate n 0) ++ (take ((length ds) - n) ds) }
 
-
 -- Take a list of patterns, mux them together, return a Maybe [Double] that we can render
 -- If they are different lengths repeat the shorter
 -- to the length of the longer
@@ -123,7 +135,7 @@ main :: IO ()
 main =
     do
         -- let outWav = fromJust $ render outPattern
-        let outWav = genWaveFile [fromJust $ stack [p1, p4], fromJust $ stack [p1, p4,p1, p3], fromJust $ hardFlange 1000 $ stack [p1, p4], fromJust $ hardFlange 750 $ stack [p3, p2], fromJust $ hardFlange 500 $ chain [p2]]
+        let outWav = genWaveFile [fromJust $ stack [p1, p4], fromJust $ stack [p1, p4,p1, p3], fromJust $ hardFlange 1000 $ stack [p1, p4],fromJust $ stack [p1,p2,p5], fromJust $ hardFlange 750 $ stack [p5, p2],  fromJust $ hardFlange 750 $ stack [p3, p2], fromJust $ hardFlange 500 $ chain [p2]]
 
         putWAVEFile "out.wav" outWav 
         -- pid <- runCommand "aplay -q out.wav"
